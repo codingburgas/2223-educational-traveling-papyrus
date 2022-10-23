1698cf6394252fad2a800e55f9e17b1be67b3584
PRDaylyanov20 <PRDaylyanov20@codingburgas.bg>
2022-10-23 16:36:32 +0300

    Add child classes for every country

--- papyrus/papyrus/papyrus/Country.h
+++ papyrus/papyrus/papyrus/Country.h
@@ -3,10 +3,11 @@
 #include <raylib.h>
 #include <vector>
 
+
 class Country
 {
 public:
-	Country(std::string Name, Image image, std::vector<std::string> offices, std::vector<std::string> exports);
+	Country(std::string Name, Image sprite, std::vector<std::string> offices, std::vector<std::string> exports);
 
 	void DrawCountry();
 	void UpgradeOffices();
@@ -16,3 +17,338 @@ private:
 	bool isClicked;
 };
 
+class Germany : public Country
+{
+public:
+	Germany() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Germany";
+	Image sprite;
+	std::vector<std::string> offices{"Berlin", "Frankfurt", "Hamburg"};
+	std::vector<std::string> exports{ "car", "food", "steel" };
+};
+
+class France : public Country
+{
+public:
+	France() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "France";
+	Image sprite;
+	std::vector<std::string> offices{ "Paris", "Lyon" };
+	std::vector<std::string> exports{ "car", "pharmaceutical"};
+};
+
+class Spain : public Country
+{
+public:
+	Spain() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Spain";
+	Image sprite;
+	std::vector<std::string> offices{ "Madrid", "Barcelona" };
+	std::vector<std::string> exports{ "food", "car" };
+};
+
+class Portugal : public Country
+{
+public:
+	Portugal() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Portugal";
+	Image sprite;
+	std::vector<std::string> offices{ "Lisbon" };
+	std::vector<std::string> exports{ "car" };
+};
+
+class UK : public Country
+{
+public:
+	UK() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "UK";
+	Image sprite;
+	std::vector<std::string> offices{ "London", "Manchester" };
+	std::vector<std::string> exports{ "car", "machinery" };
+};
+
+class Ireland : public Country
+{
+public:
+	Ireland() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Ireland";
+	Image sprite;
+	std::vector<std::string> offices{ "Dublin" };
+	std::vector<std::string> exports{ "machinery" };
+};
+
+class Iceland : public Country
+{
+public:
+	Iceland() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Iceland";
+	Image sprite;
+	std::vector<std::string> offices{ "Reykjavik" };
+	std::vector<std::string> exports{ "fish" };
+};
+
+class Norway : public Country
+{
+public:
+	Norway() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Norway";
+	Image sprite;
+	std::vector<std::string> offices{ "Oslo" };
+	std::vector<std::string> exports{ "oil" };
+};
+
+class Sweden : public Country
+{
+public:
+	Sweden() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Sweden";
+	Image sprite;
+	std::vector<std::string> offices{ "stockholm" };
+	std::vector<std::string> exports{ "car" };
+};
+
+class Denmark : public Country
+{
+public:
+	Denmark() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Denmark";
+	Image sprite;
+	std::vector<std::string> offices{ "Copnehagen" };
+	std::vector<std::string> exports{ "food" };
+};
+
+class Benelux : public Country
+{
+public:
+	Benelux() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Benelux";
+	Image sprite;
+	std::vector<std::string> offices{ "Amsterdam" };
+	std::vector<std::string> exports{ "car" };
+};
+
+class Swiss : public Country
+{
+public:
+	Swiss() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Swiss";
+	Image sprite;
+	std::vector<std::string> offices{ "Bern" };
+	std::vector<std::string> exports{ "machinery" };
+};
+
+class Austria : public Country
+{
+public:
+	Austria() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Austria";
+	Image sprite;
+	std::vector<std::string> offices{ "Vienna" };
+	std::vector<std::string> exports{ "machinery" };
+};
+
+class Poland : public Country
+{
+public:
+	Poland() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Poland";
+	Image sprite;
+	std::vector<std::string> offices{ "Warsaw" };
+	std::vector<std::string> exports{ "car" };
+};
+
+class Slovakia : public Country
+{
+public:
+	Slovakia() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Slovakia";
+	Image sprite;
+	std::vector<std::string> offices{ "Bratislava" };
+	std::vector<std::string> exports{ "car" };
+};
+
+class Hungary : public Country
+{
+public:
+	Hungary() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Hungary";
+	Image sprite;
+	std::vector<std::string> offices{ "Budapest" };
+	std::vector<std::string> exports{ "car" };
+};
+
+class Serbia : public Country
+{
+public:
+	Serbia() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Serbia";
+	Image sprite;
+	std::vector<std::string> offices{ "Belgrade" };
+	std::vector<std::string> exports{ "car" };
+};
+
+class Bulgaria : public Country
+{
+public:
+	Bulgaria() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Bulgaria";
+	Image sprite;
+	std::vector<std::string> offices{ "Sofia" };
+	std::vector<std::string> exports{ "machinery" };
+};
+
+class Greece : public Country
+{
+public:
+	Greece() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Greece";
+	Image sprite;
+	std::vector<std::string> offices{ "Athens" };
+	std::vector<std::string> exports{ "oil" };
+};
+
+class Romania: public Country
+{
+public:
+	Romania() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Romania";
+	Image sprite;
+	std::vector<std::string> offices{ "Bucharest" };
+	std::vector<std::string> exports{ "car" };
+};
+
+class Czechia : public Country
+{
+public:
+	Czechia() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Czechia";
+	Image sprite;
+	std::vector<std::string> offices{ "Prague" };
+	std::vector<std::string> exports{ "car" };
+};
+
+class Finland : public Country
+{
+public:
+	Finland() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Finland";
+	Image sprite;
+	std::vector<std::string> offices{ "Helsinki" };
+	std::vector<std::string> exports{ "machinery" };
+};
+
+class Baltic : public Country
+{
+public:
+	Baltic() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Baltic";
+	Image sprite;
+	std::vector<std::string> offices{ "Riga" };
+	std::vector<std::string> exports{ "machinery" };
+};
+
+class Italy : public Country
+{
+public:
+	Italy() : Country(Name, sprite, offices, exports)
+	{
+
+	}
+private:
+	std::string Name = "Italy";
+	Image sprite;
+	std::vector<std::string> offices{ "Rome", "Milan"};
+	std::vector<std::string> exports{ "machinery", "pharmaceutical"};
+};
