1994a4d8e699aa62145e90457be6768efdc3c697
PRDaylyanov20 <PRDaylyanov20@codingburgas.bg>
2022-10-23 18:54:27 +0300

    Create base Stock class

--- papyrus/papyrus/papyrus/Stock.h
+++ papyrus/papyrus/papyrus/Stock.h
@@ -1,11 +1,97 @@
 #pragma once
+#include <string>
 
 class Stock
 {
 public:
-	Stock();
-	~Stock();
+	Stock(std::string Name, int Price);
+
+
+	void SelectStock();
+	void Export();
+	void Produce();
 
 private:
+	bool isSelected = false;
 
 };
+
+
+class Car : public Stock
+{
+public:
+	Car() : Stock(Name, Price)
+	{
+
+	}
+private:
+	std::string Name = "Car";
+	int Price;
+	int amount = 0;
+};
+
+class Food : public Stock
+{
+public:
+	Food() : Stock(Name, Price)
+	{
+
+	}
+private:
+	std::string Name = "Food";
+	int Price;
+	int amount = 0;
+};
+
+class Steel : public Stock
+{
+public:
+	Steel() : Stock(Name, Price)
+	{
+
+	}
+private:
+	std::string Name = "Steel";
+	int Price;
+	int amount = 0;
+};
+
+class Pharmaceutical : public Stock
+{
+public:
+	Pharmaceutical() : Stock(Name, Price)
+	{
+
+	}
+private:
+	std::string Name = "Pharmaceutical";
+	int Price;
+	int amount = 0;
+};
+
+class Machinery : public Stock
+{
+public:
+	Machinery() : Stock(Name, Price)
+	{
+
+	}
+private:
+	std::string Name = "Machinery";
+	int Price;
+	int amount = 0;
+};
+
+class Oil : public Stock
+{
+public:
+	Oil() : Stock(Name, Price)
+	{
+
+	}
+private:
+	std::string Name = "Oil";
+	int Price;
+	int amount = 0;
+};
+
